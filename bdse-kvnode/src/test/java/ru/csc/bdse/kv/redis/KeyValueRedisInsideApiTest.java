4fbcf45457e582a2a16919fab458431570fa292b
darkpeaceduck <okay123456@yandex.ru>
2018-02-28 04:21:29 +0300

    Added simple startstop test to redis container start stop

---
+++ bdse-kvnode/src/test/java/ru/csc/bdse/kv/redis/KeyValueRedisInsideApiTest.java
@@ -0,0 +1,28 @@
+package ru.csc.bdse.kv.redis;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import ru.csc.bdse.kv.NodeStatus;
+
+import static org.junit.Assert.*;
+
+public class KeyValueRedisInsideApiTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    private KeyValueRedisInsideApi api;
+
+    @Before
+    public void setUp() throws Exception {
+        api = new KeyValueRedisInsideApi();
+    }
+    @Test
+    public void startStopExistingTest() throws Exception {
+        assertEquals(api.getStatus() , NodeStatus.DOWN);
+        api.upRedis();
+        assertEquals(api.getStatus() , NodeStatus.UP);
+        api.stopRedis();
+        assertEquals(api.getStatus() , NodeStatus.DOWN);
+    }
+}
