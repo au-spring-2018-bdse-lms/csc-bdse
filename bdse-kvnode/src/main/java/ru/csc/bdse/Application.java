086b95b7273d57f6ed03304a01ca6c75657554dc
Egor Suvorov <egor.suvorov@gmail.com>
2018-03-01 23:57:54 +0300

    Update Application, Env

--- bdse-kvnode/src/main/java/ru/csc/bdse/Application.java
+++ bdse-kvnode/src/main/java/ru/csc/bdse/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import ru.csc.bdse.kv.InMemoryKeyValueApi;
 import ru.csc.bdse.kv.KeyValueApi;
+import ru.csc.bdse.kv.redis.RedisKeyValueApi;
 import ru.csc.bdse.util.Env;
 
 import java.util.UUID;
@@ -23,6 +24,13 @@ public class Application {
     @Bean
     KeyValueApi node() {
         String nodeName = Env.get(Env.KVNODE_NAME).orElseGet(Application::randomNodeName);
-        return new InMemoryKeyValueApi(nodeName);
+        if (Env.get(Env.KVNODE_INMEMORY).orElse("") != "") {
+            return new InMemoryKeyValueApi(nodeName);
+        }
+        String redisUri = Env.get(Env.KVNODE_REDIS_URI).orElse("");
+        if (redisUri != "") {
+            return new RedisKeyValueApi(nodeName, redisUri);
+        }
+        throw new IllegalArgumentException("Neither KVNODE_INMEMORY nor KVNODE_REDIS_URI envvar is specified");
     }
 }
--- bdse-kvnode/src/main/java/ru/csc/bdse/util/Env.java
+++ bdse-kvnode/src/main/java/ru/csc/bdse/util/Env.java
@@ -13,6 +13,10 @@ public class Env {
 
     public static final String KVNODE_NAME = "KVNODE_NAME";
 
+    public static final String KVNODE_INMEMORY = "KVNODE_INMEMORY";
+
+    public static final String KVNODE_REDIS_URI = "KVNODE_REDIS_URI";
+
     public static Optional<String> get(final String name) {
         return Optional.ofNullable(System.getenv(name));
     }
