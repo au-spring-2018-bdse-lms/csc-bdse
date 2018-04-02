4f7dd31b287d0e3c7f9ae660cbb8265c6f280b5e
IgorL <Labutin.IgorL@gmail.com>
2018-04-02 23:33:23 +0300

    Changed tests to test http API with Redis

--- bdse-integration-tests/src/test/java/ru/csc/bdse/kv/KeyValueApiHttpClientTest.java
+++ bdse-integration-tests/src/test/java/ru/csc/bdse/kv/KeyValueApiHttpClientTest.java
@@ -1,7 +1,8 @@
 package ru.csc.bdse.kv;
 
-import org.junit.ClassRule;
+import org.junit.BeforeClass;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 import ru.csc.bdse.util.Env;
 
@@ -14,17 +15,31 @@ import static java.time.temporal.ChronoUnit.SECONDS;
  * @author semkagtn
  */
 public class KeyValueApiHttpClientTest extends AbstractKeyValueApiTest {
+    private static final int REDIS_PORT = 6379;
+    private static final Network network = Network.newNetwork();
+    private static GenericContainer redisNode;
+    private static GenericContainer node;
 
-    @ClassRule
-    public static final GenericContainer node = new GenericContainer(
-            new ImageFromDockerfile()
-                    .withFileFromFile("target/bdse-kvnode-0.0.1-SNAPSHOT.jar", new File
-                            ("../bdse-kvnode/target/bdse-kvnode-0.0.1-SNAPSHOT.jar"))
-                    .withFileFromClasspath("Dockerfile", "kvnode/Dockerfile"))
-            .withEnv(Env.KVNODE_NAME, "node-0")
-            .withEnv(Env.KVNODE_INMEMORY, "memory")
-            .withExposedPorts(8080)
-            .withStartupTimeout(Duration.of(30, SECONDS));
+    @BeforeClass
+    public static void setUp() {
+        redisNode = new GenericContainer("redis:3.2.11")
+                .withExposedPorts(REDIS_PORT)
+                .withNetwork(network)
+                .withNetworkAliases("redis")
+                .withStartupTimeout(Duration.of(30, SECONDS));
+        redisNode.start();
+        node = new GenericContainer(
+                new ImageFromDockerfile()
+                        .withFileFromFile("target/bdse-kvnode-0.0.1-SNAPSHOT.jar", new File
+                                ("../bdse-kvnode/target/bdse-kvnode-0.0.1-SNAPSHOT.jar"))
+                        .withFileFromClasspath("Dockerfile", "kvnode/Dockerfile"))
+                .withEnv(Env.KVNODE_NAME, "node-0")
+                .withEnv(Env.KVNODE_REDIS_URI, "redis://redis:" + REDIS_PORT)
+                .withExposedPorts(8080)
+                .withNetwork(network)
+                .withStartupTimeout(Duration.of(30, SECONDS));
+        node.start();
+    }
 
     @Override
     protected KeyValueApi newKeyValueApi() {
