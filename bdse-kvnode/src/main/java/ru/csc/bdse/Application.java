d8261bf868ff5703e3d55e2410d2aa0713ae309a
Egor Suvorov <egor.suvorov@gmail.com>
2018-04-06 00:19:10 +0300

    Add KVNODE_REPLICAS_URL and similar parameters to bdse-kvnode; now it can start Coordinator as well as inner node

--- bdse-kvnode/src/main/java/ru/csc/bdse/Application.java
+++ bdse-kvnode/src/main/java/ru/csc/bdse/Application.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import ru.csc.bdse.kv.InMemoryKeyValueApi;
 import ru.csc.bdse.kv.KeyValueApi;
+import ru.csc.bdse.kv.KeyValueApiHttpClient;
+import ru.csc.bdse.kv.distributed.ClusterConfiguration;
+import ru.csc.bdse.kv.distributed.Coordinator;
+import ru.csc.bdse.kv.distributed.LastTimestampConflictResolver;
 import ru.csc.bdse.kv.redis.RedisKeyValueApi;
 import ru.csc.bdse.util.Env;
 
-import java.util.UUID;
+import java.util.*;
 
 @SpringBootApplication
 public class Application {
@@ -22,7 +26,25 @@ public class Application {
     }
 
     @Bean
-    KeyValueApi node() {
+    KeyValueApi exposedNode() {
+        String[] replicasUrl = Env.get(Env.KVNODE_REPLICAS_URL).orElse("").split(",");
+        KeyValueApi innerNode = innerNode();
+        if (replicasUrl.length == 0) {
+            return innerNode;
+        }
+        int replicaTimeoutMs = Integer.parseInt(Env.get(Env.KVNODE_REPLICA_TIMEOUT_MS).orElse("1000"));
+        int wcl = Integer.parseInt(Env.get(Env.KVNODE_WCL).orElse(Integer.toString(replicasUrl.length)));
+        int rcl = Integer.parseInt(Env.get(Env.KVNODE_RCL).orElse(Integer.toString(replicasUrl.length)));
+
+        List<KeyValueApi> peers = new ArrayList<KeyValueApi>();
+        peers.add(innerNode);
+        for (String peerUrl : replicasUrl) {
+            peers.add(new KeyValueApiHttpClient(peerUrl));
+        }
+        return new Coordinator(new ClusterConfiguration(peers, 1000, wcl, rcl), new LastTimestampConflictResolver());
+    }
+
+    KeyValueApi innerNode() {
         String nodeName = Env.get(Env.KVNODE_NAME).orElseGet(Application::randomNodeName);
         if (!Env.get(Env.KVNODE_INMEMORY).orElse("").equals("")) {
             return new InMemoryKeyValueApi(nodeName);
